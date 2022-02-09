36e587113a468496fc8f6abbe094a6eb56d50f0b
graemebyrne_89@hotmail.com <graemebyrne_89@hotmail.com>
2022-02-09 12:25:03 +0000

    meme generator in react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header'
+import Meme from './components/Meme'
+
+import Style from '../src/style.css'
+
+export default function App(){
+    return(
+        <div>
+            <Header />
+            <Meme />
+        </div>
+    )
+}
